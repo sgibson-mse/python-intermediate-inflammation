aa420c6c0c43ab81a411afbd0fba7b06678cd3d3
Sam Gibson <sgibson.physicist@gmail.com>
2022-02-02 13:06:24 +0000

    Added serializer classes to store data to JSON files

---
+++ inflammation/serializers.py
@@ -0,0 +1,72 @@
+from abc import ABC
+
+from inflammation import models
+import json
+
+
+class Serializer:
+    @classmethod
+    def serialize(cls, instances):
+        raise NotImplementedError
+
+    @classmethod
+    def save(cls, instances, path):
+        raise NotImplementedError
+
+    @classmethod
+    def deserialize(cls, data):
+        raise NotImplementedError
+
+    @classmethod
+    def load(cls, path):
+        raise NotImplementedError
+
+
+class ObservationSerializer(Serializer, ABC):
+
+    model = models.Observation
+
+    @classmethod
+    def serialize(cls, instances):
+        return [{
+            'day': instance.day,
+            'value': instance.value,
+        } for instance in instances]
+
+    @classmethod
+    def deserialize(cls, data):
+        return [cls.model(**d) for d in data]
+
+
+class PatientSerializer(Serializer):
+    model = models.Patient
+
+    @classmethod
+    def serialize(cls, instances):
+        raise NotImplementedError
+
+    @classmethod
+    def save(cls, instances, path):
+        raise NotImplementedError
+
+    @classmethod
+    def deserialize(cls, data):
+        raise NotImplementedError
+
+    @classmethod
+    def load(cls, path):
+        raise NotImplementedError
+
+
+class PatientJSONSerializer(PatientSerializer, ABC):
+    @classmethod
+    def save(cls, instances, path):
+        with open(path, 'w') as jsonfile:
+            json.dump(cls.serialize(instances), jsonfile)
+
+    @classmethod
+    def load(cls, path):
+        with open(path) as jsonfile:
+            data = json.load(jsonfile)
+
+        return cls.deserialize(data)
